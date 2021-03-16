33786211d0f15bb1a1dbd547b9c199ee724e1fec
Daniil Kamakin <cursedowguy@gmail.com>
2021-03-17 00:13:22 +0300

    upload/download done with small changes. TODO: list of uploaded files

--- src/main/java/com/dell/ecloud/FileController.java
+++ src/main/java/com/dell/ecloud/FileController.java
@@ -1,12 +1,12 @@
 package com.dell.ecloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class FileController {
@@ -23,13 +23,17 @@ public class FileController {
         return "main";
     }
 
+    @GetMapping("/uploads/{filename}")
+    @ResponseBody
+    public ResponseEntity<Resource> fileDownload(@PathVariable("filename") String fileName) {
+        Resource file = storageService.toResource(fileName);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
     @PostMapping("/upload")
-    public String fileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-
+    public String fileUpload(@RequestParam("file") MultipartFile file) {
         storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "File " + file.getOriginalFilename() + " was successfully uploaded");
-
         return "redirect:/";
     }
 
--- src/main/java/com/dell/ecloud/FileStorageService.java
+++ src/main/java/com/dell/ecloud/FileStorageService.java
@@ -1,11 +1,13 @@
 package com.dell.ecloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,7 +17,7 @@ import java.util.Objects;
 @Service
 public class FileStorageService {
 
-    private final Path rootLocation = Path.of("test");
+    private final Path rootPath = Paths.get("uploads");
 
     @Autowired
     public FileStorageService() {}
@@ -26,14 +28,32 @@ public class FileStorageService {
                 throw new IOException("Error: the file is empty");
             }
 
-            Path destinationFile = this.rootLocation.resolve(
-                    Paths.get(Objects.requireNonNull(file.getOriginalFilename()))
-                    .normalize().toAbsolutePath());
+            Path destinationFile = this.rootPath.resolve(Paths
+                    .get(Objects.requireNonNull(file.getOriginalFilename()))
+                    .normalize()).toAbsolutePath();
 
             Files.copy(file.getInputStream(), destinationFile, StandardCopyOption.REPLACE_EXISTING);
         }
         catch (IOException e) {
             System.out.println("Failed to store the file. " + e.getMessage());
+        }
+    }
+
+    public Resource toResource(String fileName) {
+        try {
+            Path file = this.rootPath.resolve(Paths
+                    .get(fileName).normalize()).toAbsolutePath();
+
+            Resource resource = new UrlResource(file.toUri());
+            System.out.println(file.getParent());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new MalformedURLException("Error: wrong path");
+            }
+        } catch (MalformedURLException e) {
+            System.out.println("Failed to return the file. " + e.getMessage());
+            return null;
         }
     }
 
