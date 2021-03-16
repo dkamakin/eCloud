ea3eb481cbaa4fb0e9f0defb2958aebcd8512a25
Daniil Kamakin <cursedowguy@gmail.com>
2021-03-16 11:52:47 +0300

    init

---
+++ src/main/java/com/dell/ecloud/ECloudApplication.java
@@ -0,0 +1,13 @@
+package com.dell.ecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECloudApplication.class, args);
+	}
+
+}
